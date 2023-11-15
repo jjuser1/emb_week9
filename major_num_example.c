4cffef8d85d8f755094f7f094772792fcb4ed4c8
heung8198 <107354972+heung8198@users.noreply.github.com>
2023-11-15 21:08:57 +0900

    Update major_num_example.c

--- major_num_example.c
+++ major_num_example.c
@@ -7,12 +7,9 @@ int main(){
 	int dev = open("dev/mydevice", O_RDONLY);
 	if(dev == -1){
 		printf("Opening was not possible!\n");
-		return -1;}
-
+		return -1;
+	}
 	printf("Opening was successfull!\n");
 	close(dev);
 	return 0;
-		
-
-
 }
